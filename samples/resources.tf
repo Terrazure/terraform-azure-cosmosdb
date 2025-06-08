d7931b765861461240c4acf3643a48e926eb0945
PinakiKundu <pinakispecial@gmail.com>
2025-06-08 12:17:41 +0530

    - test config

--- samples/resources.tf
+++ samples/resources.tf
@@ -25,5 +25,8 @@ resource "azurerm_subnet" "snet" {
   resource_group_name  = azurerm_resource_group.group.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-  service_endpoints    = ["Microsoft.EventHub"]
+
+  service_endpoints = [
+    "Microsoft.AzureCosmosDB"
+  ]
 }
